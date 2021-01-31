81175c4c25a2967696302b88514b418c895f2fe4
baohan <baohan@tips.com>
2021-02-01 00:27:23 +0800

    +

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,20 @@
 import styles from './index.less';
+import { useState } from 'react';
 
 export default function IndexPage() {
+  const [state, setstate] = useState(0);
+  console.log(state, '111');
   return (
     <div>
-      <h1 className={styles.title}>Page index</h1>
+      <h1>{state}</h1>
+      <h1
+        className={styles.title}
+        onClick={() => {
+          setstate(state + 1);
+        }}
+      >
+        Page index
+      </h1>
     </div>
   );
 }
